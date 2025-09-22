008e4184c9ef84e39c188e73eab92e59d7c41334
Ashraf Hafiz <ashraf.hafiz@yahoo.com>
2025-09-22 16:22:50 +0300

    #03 - 22092025 - 04:00 PM

--- app/Filament/Resources/Notes/Schemas/NoteInfolist.php
+++ app/Filament/Resources/Notes/Schemas/NoteInfolist.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Notes\Schemas;
 use App\Models\Note;
 use Filament\Infolists\Components\IconEntry;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class NoteInfolist
@@ -13,23 +14,44 @@ class NoteInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('title'),
-                TextEntry::make('content')
-                    ->placeholder('-')
-                    ->columnSpanFull(),
-                IconEntry::make('active')
-                    ->boolean(),
-                TextEntry::make('user_id')
-                    ->numeric(),
-                TextEntry::make('deleted_at')
-                    ->dateTime()
-                    ->visible(fn (Note $record): bool => $record->trashed()),
-                TextEntry::make('created_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('updated_at')
-                    ->dateTime()
-                    ->placeholder('-'),
+                Section::make('System Info')
+                    ->inlineLabel()
+                    ->description('System information about the note.')
+                    ->schema([
+                        TextEntry::make('deleted_at')
+                            ->dateTime()
+                            ->visible(fn(Note $record): bool => $record->trashed()),
+                        TextEntry::make('created_at')
+                            ->dateTime()
+                            ->placeholder('-'),
+                        TextEntry::make('updated_at')
+                            ->dateTime()
+                            ->placeholder('-'),
+                    ])
+                    ->collapsible(),
+                Section::make('Status Info')
+                    ->inlineLabel()
+                    ->description('Status information about the note.')
+                    ->schema([
+                        IconEntry::make('active')
+                            ->boolean(),
+                        TextEntry::make('user.name')
+                            ->label('Author')
+                            ->numeric(),
+                    ])
+                    ->collapsible(),
+                Section::make('Note')
+                    ->translateLabel()
+                    ->description('Information about the note.')
+                    ->schema([
+                        TextEntry::make('title')
+                            ->extraAttributes(['class' => 'text-2xl font-bold']),
+                        TextEntry::make('content')
+                            ->markdown()
+                            ->placeholder('-')
+                    ])
+                    ->columnSpanFull()
+                    ->collapsible(),
             ]);
     }
 }
